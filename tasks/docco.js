0625b769718465fe8ed8cc1273f32cf868725970
D1plo1d <d1plo1d@d1plo1d.com>
2013-12-05 20:40:52 -0500

    Fixing paths issue (html files were getting stored one folder too deep).

--- tasks/docco.js
+++ tasks/docco.js
@@ -6,20 +6,22 @@
 
 "use strict";
 var docco = require('docco');
+var path = require('path');
 
 module.exports = function(grunt) {
   grunt.registerMultiTask('docco', 'Docco processor.', function() {
     var self = this;
+
     // onDoccoComplete waits until all files are processed to finish the task
     var done = this.async();
     var remainingFiles = 0;
     var onDoccoComplete = function() {
       if (remainingFiles-- == 0) done();
     };
-    console.log("a");
+
     this.files.forEach(function(file) {
       remainingFiles++;
-      var options = {output: file.dest, args: [file.src[0]]};
+      var options = {output: path.dirname(file.dest), args: [file.src[0]]};
       for (k in self.options) options[k] = self.options[k];
       docco.document(options, onDoccoComplete);
     });
